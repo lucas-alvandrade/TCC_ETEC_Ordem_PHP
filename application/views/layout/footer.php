77bab19d31e312d53bf121b6859485c4b7f4cc09
Lucas Andrade <lucas.alvandrade@gmail.com>
2021-07-06 07:38:34 -0300

    Adicionado execução dinâmica dos arquivos CSS e javascript

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -2,7 +2,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>Copyright &copy; System Ordem <?php echo date('Y') ?>&nbsp; | By GRUPO 7</span>
                     </div>
                 </div>
             </footer>
@@ -49,6 +49,17 @@
     <!-- Custom scripts for all pages-->
     <script src="<?php echo base_url('public/js/sb-admin-2.min.js'); ?>"></script>
 
+    <?php if(isset($scripts)): ?>
+    
+        <?php foreach ($scripts as $script): ?>
+            
+            <!-- Custom scripts for this module-->
+            <script src="<?php echo base_url('public/'.$script); ?>"></script>
+    
+        <?php endforeach; ?>
+    
+    <?php endif; ?>
+    
 </body>
 
 </html>
