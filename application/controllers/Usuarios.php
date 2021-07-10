c69f5a80cbb93cd2b1912065fc3842588ee0fcb4
Lucas Andrade <lucas.alvandrade@gmail.com>
2021-07-10 16:44:58 -0300

    Adicionado tela Cadastro de Usuários

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -28,6 +28,61 @@ class Usuarios extends CI_Controller {
         $this->load->view('layout/footer');
     }
 
+    public function add() {
+
+        $this->form_validation->set_rules('first_name', '', 'trim|required');
+        $this->form_validation->set_rules('last_name', '', 'trim|required');
+        $this->form_validation->set_rules('email', '', 'trim|required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('username', '', 'trim|required|is_unique[users.username]');
+        $this->form_validation->set_rules('password', 'Senha', 'required|min_length[5]|max_length[255]');
+        $this->form_validation->set_rules('confirm_password', 'Confirme', 'matches[password]');
+
+        if ($this->form_validation->run()) {
+
+            $username = $this->security->xss_clean($this->input->post('username'));
+            $password = $this->security->xss_clean($this->input->post('password'));
+            $email = $this->security->xss_clean($this->input->post('email'));
+
+            $additional_data = array(
+                'first_name' => $this->input->post('first_name'),
+                'last_name' => $this->input->post('last_name'),
+                'username' => $this->input->post('username'),
+                'active' => $this->input->post('active'),
+            );
+            $group = array($this->input->post('perfil_usuario')); // Sets user to admin.
+
+            $additional_data = $this->security->xss_clean($additional_data);
+
+            $group = $this->security->xss_clean($group);
+
+            if ($this->ion_auth->register($username, $password, $email, $additional_data, $group)) {
+
+                $this->session->set_flashdata('success', 'Dados salvos com sucesso');
+            } else {
+
+                $this->session->set_flashdata('error', 'Erro ao salvar os dados');
+            }
+
+            redirect('usuarios');
+
+            //alterar ion_auth_model (linha 853) para que o valor do campo active não fique padrão
+//            echo '<pre>';
+//                print_r($additional_data);
+//                exit();
+        } else {
+
+            //Erro de validação
+
+            $data = array(
+                'titulo' => 'Cadastrar usuário',
+            );
+
+            $this->load->view('layout/header', $data);
+            $this->load->view('usuarios/add');
+            $this->load->view('layout/header');
+        }
+    }
+
     public function edit($usuario_id = NULL) {
 
         if (!$usuario_id || !$this->ion_auth->user($usuario_id)->row()) {
@@ -100,7 +155,7 @@ class Usuarios extends CI_Controller {
 
                     $this->session->set_flashdata('error', 'Erro ao salvar os dados');
                 }
-                
+
                 redirect('usuarios');
             } else {
 
--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -52,7 +52,7 @@
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <a title="Cadastrar novo usuário" href="" class="btn btn-success btn-sm float-right"><i class="fas fa-user-plus">&nbsp;Novo</i></a>
+                <a title="Cadastrar novo usuário" href="<?php echo base_url('usuarios/add')?>" class="btn btn-success btn-sm float-right"><i class="fas fa-user-plus">&nbsp;Novo</i></a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -62,7 +62,7 @@
                                 <th>#</th>
                                 <th>Usuário</th>
                                 <th>Login</th>
-                                <th>Ativo</th>
+                                <th class="text-center">Ativo</th>
                                 <th class="text-right no-sort">Ações</th>
                             </tr>
                         </thead>
@@ -72,7 +72,7 @@
                                     <td><?php echo $user->id ?></td>
                                     <td><?php echo $user->username ?></td>
                                     <td><?php echo $user->email ?></td>
-                                    <td><?php echo $user->active ?></td>
+                                    <td class="text-center pr-4"><?php echo ($user->active == 1 ? '<span class="badge badge-info btn-sm">Sim</span>' : '<span class="badge badge-warning btn-sm">Não</span>') ?></td>
                                     <td class="text-right">
                                         <a title="Editar" href="<?php echo base_url('usuarios/edit/' . $user->id) ?>" class="btn btn-sm btn-primary"><i class="fas fa-user-edit"></i></a>
                                         <a title="Excluir" href="" class="btn btn-sm btn-danger"><i class="fas fa-user-times"></i></a>
