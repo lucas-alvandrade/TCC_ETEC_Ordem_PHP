ec809100e617f328b0d97068ddde00b449f2cefe
Lucas Andrade <lucas.alvandrade@gmail.com>
2021-07-11 06:05:22 -0300

    Adicionado Exclusão de Usuários

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -172,6 +172,27 @@ class Usuarios extends CI_Controller {
         }
     }
 
+    public function del($usuario_id = NULL) {
+
+        if (!$usuario_id || !$this->ion_auth->user($usuario_id)->row()) {
+            $this->session->set_flashdata('error', 'Usuário não encontrado');
+            redirect('usuarios');
+        }
+        
+        if($this->ion_auth->is_admin($usuario_id)){
+            $this->session->set_flashdata('error', 'O administrador não pode ser excluído');
+            redirect('usuarios');
+        }
+        
+        if($this->ion_auth->delete_user($usuario_id)){
+            $this->session->set_flashdata('success', 'Usuário excluído com sucesso');
+            redirect('usuarios');
+        }else{
+            $this->session->set_flashdata('error', 'O administrador não pode ser excluído');
+            redirect('usuarios');
+        }
+    }
+
     public function email_check($email) {
 
         $usuario_id = $this->input->post('usuario_id');
--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -38,7 +38,7 @@
 
             <div class="row">
                 <div class="col-md-12">
-                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
                         <strong><i class="fas fa-exclamation-triangle"></i>&nbsp;&nbsp;<?php echo $message; ?></strong>
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
@@ -52,7 +52,7 @@
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <a title="Cadastrar novo usuário" href="<?php echo base_url('usuarios/add')?>" class="btn btn-success btn-sm float-right"><i class="fas fa-user-plus">&nbsp;Novo</i></a>
+                <a title="Cadastrar novo usuário" href="<?php echo base_url('usuarios/add') ?>" class="btn btn-success btn-sm float-right"><i class="fas fa-user-plus">&nbsp;Novo</i></a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -62,6 +62,7 @@
                                 <th>#</th>
                                 <th>Usuário</th>
                                 <th>Login</th>
+                                <th>Perfil</th>
                                 <th class="text-center">Ativo</th>
                                 <th class="text-right no-sort">Ações</th>
                             </tr>
@@ -72,13 +73,34 @@
                                     <td><?php echo $user->id ?></td>
                                     <td><?php echo $user->username ?></td>
                                     <td><?php echo $user->email ?></td>
+                                    <td><?php echo ($this->ion_auth->is_admin($user->id) ? 'Administrador' : 'Vendedor'); ?></td>
                                     <td class="text-center pr-4"><?php echo ($user->active == 1 ? '<span class="badge badge-info btn-sm">Sim</span>' : '<span class="badge badge-warning btn-sm">Não</span>') ?></td>
                                     <td class="text-right">
                                         <a title="Editar" href="<?php echo base_url('usuarios/edit/' . $user->id) ?>" class="btn btn-sm btn-primary"><i class="fas fa-user-edit"></i></a>
-                                        <a title="Excluir" href="" class="btn btn-sm btn-danger"><i class="fas fa-user-times"></i></a>
+                                        <a title="Excluir" href="javascript(void)" data-toggle="modal" data-target="#user-<?php echo $user->id; ?>" class="btn btn-sm btn-danger"><i class="fas fa-user-times"></i></a>
                                     </td>
                                 </tr>
-                            <?php endforeach; ?>
+
+                            <div class="modal fade" id="user-<?php echo $user->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                                 aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Deseja excluir este usuário?</h5>
+                                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">Caso deseje continuar, clique em <strong>"Sim"</strong></div>
+                                        <div class="modal-footer">
+                                            <button class="btn btn-secondary btn-sm" type="button" data-dismiss="modal">Não</button>
+                                            <a class="btn btn-danger btn-sm" href="<?php echo base_url('usuarios/del/' . $user->id); ?>">Sim</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
